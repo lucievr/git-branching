198a7c79fcefb70e5f222112974dc9c3fc1f3ade
Lucie <46979603+lucievr@users.noreply.github.com>
2019-05-29 23:18:39 +0200

    Add more text

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>change</title>
-
-    <link rel="stylesheet" href="./style.css">
+    <title>Git Branching Test</title>
 </head>
 <body>
     <h1>The best page ever</h1>
